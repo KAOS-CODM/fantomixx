6fb7fefdb8e2d0caf84c9dcb19a5059b4d32e234
KAOS-CODM <klasikyemite04@gmail.com>
2025-06-25 20:08:36 +0100

    second commit

--- public/js/config.js
+++ public/js/config.js
@@ -4,4 +4,4 @@ const isLocalhost = window.location.hostname === "localhost";
 
 const API_BASE_URL = isLocalhost
   ? "http://localhost:3000/api"  // Your local dev server
-  : "https://yourdomain.com/api";  // Replace with your deployed domain
+  : "https://fantomixx.onrender.com/api"  // Replace with your deployed domain
